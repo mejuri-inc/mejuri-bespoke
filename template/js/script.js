b47ac1d9acbc86cae159ede3fe68d411f2e12348
Elianapiquets <elianadeluca@hotmail.com>
2022-01-18 12:21:34 -0300

    bespoke template

---
+++ template/js/script.js
@@ -0,0 +1,7 @@
+// In this file you can modify everything
+
+(function () {
+    var scene = document.getElementById('scene')
+    var parallaxInstance = new Parallax(scene)
+    parallaxInstance.friction(0.2, 0.2)
+})();
