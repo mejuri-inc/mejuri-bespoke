90d837ba90b94c271119e2ff70b5add1c639466e
Elianapiquets <elianadeluca@hotmail.com>
2022-02-16 10:34:59 -0300

    Word by word animation

--- iwd-lp/js/script.js
+++ iwd-lp/js/script.js
@@ -51,5 +51,59 @@ document.addEventListener("DOMContentLoaded", function (event) {
       });
   });
 
+   function animateWordByWord(element) {
+      const SENTENCE_DELAY = 1000;
+
+      element.innerHTML = element.textContent.split(' ').map(word => '<span class="faded-word">' + word + '</span>').join(' ');
+
+      element.classList.replace("invisible", "visible");
+
+      let wordsForFading = element.querySelectorAll('.faded-word');
+
+      wordsForFading.forEach(word => {
+         word.addEventListener('transitionend', startNextWordAnimation);
+      });
+
+      function startNextWordAnimation(e) {
+         let nextWord = e.target.nextElementSibling;
+         if (nextWord) {
+            nextWord.classList.add('faded-activated');
+         } else {
+            let nextSentence = e.target.parentElement.nextElementSibling;
+            startSentence(nextSentence);
+         }
+      }
+
+      startSentence(element);
+
+      function startSentence(sentenceElement) {
+         if (!sentenceElement) {
+            return;
+         }
+         const listItems = sentenceElement.children;
+         const listArray = Array.from(listItems);
+
+         listArray.forEach((word, index) => {
+            let delay = index * 100 + SENTENCE_DELAY
+            setTimeout(() => {
+               word.classList.add('faded-activated');
+            } , delay )         
+         });           
+      }
+   }
+
+   const intersectionObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+         if (entry.isIntersecting) {
+            animateWordByWord(entry.target)
+            observer.unobserve(entry.target);
+         }
+      });
+   });
+
+   const wordByWordElements = [...document.querySelectorAll('.word-by-word')];
+
+   wordByWordElements.forEach((element) => intersectionObserver.observe(element));
+
 });
 
